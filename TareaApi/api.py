cb21d934d07c0808a6f393b5c89582247232cc21
Carlos Alvarado <cmalvarado@ufm.edu>
2021-02-05 19:58:13 -0600

    Create api.py
    api de linear y binary search

---
+++ TareaApi/api.py
@@ -0,0 +1,47 @@
+from flask import Flask
+import random
+app = Flask(__name__)
+
+@app.route('/linear/<int:N>/<int:Values>')
+def linea_search(N=None,Values=None):
+    if(N > 100):
+        lista = random.sample(range(1, (50+Values)), N)
+    else:
+        lista = random.sample(range(1, (100)), N)
+    r = "no encontrado"
+    for i in lista:
+        if(i == Values):
+            r = "Encontrado"
+            break
+    return f'La lista es: {lista}\n Numero a buscar: {Values}\n En metodo lineal: {r}'
+
+@app.route('/binary/<int:N>/<int:Values>')
+def binary_search(N=None,Values=None):
+    if(N > 100):
+        lista = random.sample(range(1, (50+Values)), N)
+    else:
+        lista = random.sample(range(1, (100)), N)
+    r = "no encontrado"
+    ls = len(lista)-1
+    li = 0
+    me = int(len(lista)/2)
+    lista.sort()
+    while (r == "no encontrado" and me > li and me < ls):
+        if(Values <= lista[ls] and Values >= lista[me]):
+            if(Values == lista[ls] or Values == lista[me]):
+                r = "Encontrado"
+            else:
+                li = me
+                me = int((li+ls)/2)
+        elif(Values >= lista[li] and Values < lista[me]):
+            if(Values == lista[li]):
+                r = "Encontrado"
+            else:
+                ls = me
+                me = int((li+ls)/2)
+        else:
+            break
+    return f'La lista es: {lista}\n Numero a buscar: {Values}\n En metodo binario: {r}'
+
+if __name__ == '__main__':
+    app.run()
