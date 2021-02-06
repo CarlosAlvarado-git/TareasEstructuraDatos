daaa70c86814524b05e4150fcfec5d1e162e98af
Carlos Alvarado <cmalvarado@ufm.edu>
2021-02-05 23:10:48 -0600

    cambios y test jmeter
    La verdad no se si hubo una mejora con el cambio y en el jmeter ya arriba de 100 usuario, la mayoría lanzaba error.

--- TareaApi/api.py
+++ TareaApi/api.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 @app.route('/linear/<int:N>/<int:Values>', methods=['GET'])
 def linea_search(N=None,Values=None):
-    if(N > 100):
-        lista = random.sample(range(1, (50+Values)), N)
-    else:
-        lista = random.sample(range(1, (100)), N)
+    i = 0
+    lista = []
+    while(i < N):
+        lista.append(random.randint(1,100))
+        i += 1
     r = "no encontrado"
     for i in lista:
         if(i == Values):
@@ -17,28 +18,29 @@ def linea_search(N=None,Values=None):
 
 @app.route('/binary/<int:N>/<int:Values>', methods=['GET'])
 def binary_search(N=None,Values=None):
-    if(N > 100):
-        lista = random.sample(range(1, (50+Values)), N)
-    else:
-        lista = random.sample(range(1, (100)), N)
+    i = 0
+    lista = []
+    while(i < N):
+        lista.append(random.randint(1,100))
+        i += 1
     r = "no encontrado"
     ls = len(lista)-1
     li = 0
-    me = int(len(lista)/2)
+    me = (li+ls)//2
     lista.sort()
     while (r == "no encontrado" and me > li and me < ls):
         if(Values <= lista[ls] and Values >= lista[me]):
             if(Values == lista[ls] or Values == lista[me]):
                 r = "Encontrado"
             else:
-                li = me
-                me = int((li+ls)/2)
+                li = me + 1 
+                me = (li+ls)//2
         elif(Values >= lista[li] and Values < lista[me]):
             if(Values == lista[li]):
                 r = "Encontrado"
             else:
-                ls = me
-                me = int((li+ls)/2)
+                ls = me + 1
+                me = (li+ls)//2
         else:
             break
     return f'La lista es: {lista}\n Numero a buscar: {Values}\n En metodo binario: {r}'
