bbaa6cc46f3208822425583fe539cf71e2d83756
Carlos Alvarado <cmalvarado@ufm.edu>
2021-02-14 19:52:19 -0600

    cuenta las letras de los arreglos
    puedo mostrar todo pero sin ordenar, ya que al ordenar perderia el ascii correcto

--- Tarea_Arrays/eje1.java
+++ Tarea_Arrays/eje1.java
@@ -6,15 +6,24 @@ public class eje1{
         System.out.print("Ingrese el numero de parrafos a contar: ");
         int numero = scan.nextInt();
         scan.nextLine();
+        System.out.println("Debe dar doble enter luego de ingresar los parrafos");
         System.out.println("Ingrese parrafo :");
         String texto = "";
+        int letras[] = new int[26];
         for(int i = 0; i < numero; i++){
-            texto += scan.nextLine();
+            texto = scan.nextLine();
             scan.nextLine();
+            for(int x = 0; x < texto.length(); x++){
+                int pos = (int) (texto.charAt(x));
+                pos = pos - 97;
+                if(pos >= 0 && pos <= 25)
+                    letras[pos] = letras[pos] + 1;
+            }
         }
-        System.out.print("\n");
-        System.out.print("\n");
-        System.out.println(texto);
+        for(int i = 0; i < 26; i++){
+            System.out.println("La cantidad de "+ (char)(i+97)+ " es: " + letras[i]);
+        }
+
         
     }
 }
